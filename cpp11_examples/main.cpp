51f4f385dbb892661c6216e0a58a835a8324233d
constant-arapov <constant_arapov@bk.ru>
2018-12-29 12:24:23 +0500

    12:24

--- cpp11_examples/main.cpp
+++ cpp11_examples/main.cpp
@@ -1,5 +1,13 @@
 #include <initializer_list>
 #include <vector>
+#include <functional>
+#include <iostream>
+#include <random>
+
+
+#include "type_interface.hpp"
+#include "main.h"
+using namespace std;
 
 struct Object
 {
@@ -38,12 +46,12 @@ int main()
 {
 	//===================  INITIALIZATION LISTS ===================================
 	//simple init
-	Object scalar = { 0.43f,0.1 };
+	Object scalar = { 0.43f, 1 };
 	Object anArray[] = { {13.4f, 3}, {43.28f, 29}, {5.934f, 17} };
 
 	//containers init
-	std::vector<std::string> v = { "123","456" };
-	v.push_back("1");
+	std::vector<std::string> vect_int = { "123","456" };
+	vect_int.push_back("1");
 	
 
 
@@ -54,5 +62,59 @@ int main()
 
 	IdString st = GetString();
 	//============END  INITIALIZATION LISTS ===================================
+
+
+	//=================== TYPE EINTERFACE ===================================
+	//auto someStrangeCallableType = std::bind(&SomeFunction, _2, _1, someObject);
+
+	//----
+	int n = 7;
+	using namespace std::placeholders;
+
+	//cref is "constant " used for putting refference to n,
+	//auto is a varriable type
+	auto f1 = std::bind(f, _2, _1, 42, std::cref(n), n);
+	n = 10;
+	auto res  = f1(1, 2, 1001);// 1 is bound by _1, 2 is bound by _2, 1001 is unused
+
+	//-----
+	auto otherVariable = "1";
+	otherVariable = "2123";
+
+	//--
+	int MyCustomIntegerType;
+	decltype(MyCustomIntegerType) otherIntegerVariable = 5;
+	
+	vector<int> myvec = {1,2,3};
+
+	//old style
+	for (vector<int>::const_iterator itr = myvec.cbegin(); itr != myvec.cend(); ++itr)
+		cout << *itr << endl;
+
+	//new style
+	for (auto itr = myvec.cbegin(); itr != myvec.cend(); ++itr)
+		cout << *itr << endl;
+
+	//--
+	const std::vector<int> v(1);
+	auto a = v[0];        // a has type int
+	decltype(v[0]) b = 1; // b has type const int&, the return type of
+						  //   std::vector<int>::operator[](size_type) const
+	auto c = 0;           // c has type int
+	auto d = c;           // d has type int
+	decltype(c) e;        // e has type int, the type of the entity named by c
+	decltype((c)) f = c;  // f has type int&, because (c) is an lvalue
+	decltype(0) g;        // g has type int, because 0 is an rvalue
+	
+
+	//=================== END TYPE EINTERFACE ===================================
+
+
+	//================== RANGE-BASED FOR LOOP ======================================
+
+
+
+	//================== END RANGE-BASED FOR LOOP ======================================
+
 	return 0;
 }
---
+++ cpp11_examples/type_interface.hpp
@@ -0,0 +1,13 @@
+#ifndef _TYPE_INTERFACE
+#define _TYPE_INTERFACE_
+
+
+int f(int n1, int n2, int n3, const int& n4, int n5)
+{
+	std::cout << n1 << ' ' << n2 << ' ' << n3 << ' ' << n4 << ' ' << n5 << '\n';
+	return 4;
+}
+
+
+
+#endif
